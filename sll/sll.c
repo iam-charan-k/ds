ce2439424f414f07ba8f4feca3eba116773e6df5
iam-charan-k <220920723+iam-charan-k@users.noreply.github.com>
2026-04-20 05:41:35 +0000

    new

--- sll/sll.c
+++ sll/sll.c
@@ -1,3 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>s
+
 typedef struct sll{
     int d;
     struct sll *next;
@@ -19,6 +23,91 @@ void add_begin(){
     new->next=head;
     head=new;
     printf("node added at begin");
+}
+
+void add_databased(){
+    SLL *new = malloc(sizeof(SLL));
+    if(new==0){
+        perror("malloc");
+        return;
+    }
+    printf("enter data:");
+    scanf("%d",&new->d);
+    new->next=0;
+
+    if(head==NULL || new->d<head->d){
+        new->next=head;
+        head=new;
+    }
+    else{
+        SLL *p=head;
+
+        while(p->next && p->next->d<new->d){
+            p=p->next;
+        }
+        new->next=p->next;
+        p->next=new;
+    }
+}
+
+void add_middle(){
+
+    SLL *new = malloc(sizeof(SLL));
+    if(new==0){
+        perror("malloc");
+        return;
+    }
+    printf("enter data:");
+    scanf("%d",&new->d);
+    new->next=0;
+
+    if(head==NULL){
+        new->next=head;
+        head=new;
+    }
+    else{
+        SLL *slow=head;
+        SLL *fast=head;
+
+        while(fast->next && fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        new->next=slow;
+        slow->next=new;
+    }
+    
+
+}
+
+void add_position(){
+    SLL *new = malloc(sizeof(SLL));
+    if(new==0){
+        perror("malloc");
+        return;
+    }
+    printf("enter data:");
+    scanf("%d",&new->d);
+    new->next=0;
+
+    int i=0;
+    int pos=0;
+    printf("enter pos:");
+    scanf("%d",&pos);
+
+    if(head==NULL || pos==1){
+        new->next=head;
+        head=new;
+    }
+    else{
+        SLL *p=head;
+        for(i=1;i<(pos-1) && p->next ;i++){
+            p=p->next;
+        }
+        new->next=p->next;
+        p->next=new;
+    }
 }
 
 void add_end(){
@@ -58,6 +147,40 @@ void delete_begin(){
 
         printf("deleted first node");
     }
+}
+
+void delete_middle(){
+   if(head==NULL){
+        printf("no nodes exist to delete returned\n");
+        return;
+   }
+   if(head->next==NULL){
+        free(head);
+        head=NULL;
+   }
+   else{
+        SLL *prev=head;
+        SLL *slow=head;
+        SLL * fast =head;
+
+        while(fast->next && fast->next){
+            fast=fast->next->next;
+            prev=slow;
+            slow=slow->next;
+        }
+        prev->next=slow->next;
+        free(slow);
+   } 
+}
+
+void delete_position(){
+     if(head==NULL){
+        printf("no nodes exist to delete returned\n");
+        return;
+   }
+   else{
+
+   }
 }
 
 void delete_end(){
@@ -125,9 +248,10 @@ void print_databased(){
 }
 
 void print_reverse(SLL *head){
-     if(head!=NULL){
-        print_reverse(head->next);
+     if(head==NULL){
+        return;
      }
+     print_reverse(head->next);
      printf("%d\n",head->d);
 }
 
@@ -148,4 +272,27 @@ void sort(){
         }
     }
     printf("data sorted\n");
+}
+
+void reverse_link(){
+    if(head==NULL){
+        printf("no nodes to print\n");
+        return;
+    }
+    
+    SLL *prev=0;
+    SLL *next=0;
+    SLL *cur=head;
+
+    while(cur){
+        
+        new=cur->next;
+        cur->next=prev;
+        prev=cur;
+        cur=next;
+
+        cur=cur->next;
+    }
+    head=prev;
+
 }
